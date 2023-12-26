4b7da9c2d9f92d190fafb63c27b0134a02a60a27
Tigran124 <tigran.h.988@gmail.com>
2023-12-26 15:42:27 +0400

    changes completed

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -3,13 +3,16 @@ package school.mjc.stage0.loops.task3;
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
         int sum = 0;
-        for (int i = 0; i < lengthOfLastNumber; i++) {
+        for (int i = 0; i <= lengthOfLastNumber; i++) {
             sum += buildNum(i);
         }
         System.out.println(sum);
     }
 
     public int buildNum(int numbLength){
+        if (numbLength == 0){
+            return 0;
+        }
         StringBuilder num = new StringBuilder();
         for (int i = 1; i <= numbLength; i++) {
             num.append("9");
